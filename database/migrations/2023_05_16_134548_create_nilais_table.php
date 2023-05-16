34548ac47ef17854b67e5410903bc2955d0723db
MasEBTA <ebta.sb@gmail.com>
2023-05-16 20:47:01 +0700

    Membuat Model&filemigrase peserta dan nilai

---
+++ database/migrations/2023_05_16_134548_create_nilais_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNilaisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nilais', function (Blueprint $table) {
+            $table->id();
+            $table->integer('nilai_x');
+            $table->integer('nilai_y');
+            $table->integer('nilai_z');
+            $table->integer('nilai_w');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nilais');
+    }
+}
